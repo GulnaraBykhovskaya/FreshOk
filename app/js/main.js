4942424c54f98a8c5666c0bdf72dc0ae5bf22719
GulnaraBykhovskaya <bykhovskaya.buro@gmail.com>
2021-12-27 21:11:30 +0200

    added section slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,8 @@
-$(function () {});
+$(function () {
+  $(".slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="Левая стрелка"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="Праввая стрелка"></button>',
+  });
+});
